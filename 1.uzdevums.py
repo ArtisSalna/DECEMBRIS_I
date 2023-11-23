be4776dc1b0f62adc958981286b8aab9d830ac01
ArtisSalna <artis.salna@grobinasskola.lv>
2023-11-23 12:30:13 +0200

    1.uzdevums

---
+++ 1.uzdevums.py
@@ -0,0 +1,11 @@
+sskaitlis = int(input("Ievadiet skaitli: "))
+
+# Sākam skaitīt no 1
+summa = 0
+
+# For cikls, lai saskaitītu skaitļus no 1 līdz ievadītajam skaitlim
+for skaitlis in range(1, sskaitlis + 1):
+    summa += skaitlis
+
+# Izdrukājam rezultātu
+print(f"Summa no 1 līdz {sskaitlis} ir: {summa}")
