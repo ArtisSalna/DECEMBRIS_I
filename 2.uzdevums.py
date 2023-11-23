b47e75c32389479e5a61d7391b3a5e4793ddce2f
ArtisSalna <artis.salna@grobinasskola.lv>
2023-11-23 12:41:48 +0200

    2.uzdevums

--- 2.uzdevums.py
+++ 2.uzdevums.py
@@ -1,4 +1,4 @@
 skaitlis=1
-while skaitlis<=1000:
-    print(skaitlis)
-    skaitlis+=1
+while skaitlis**2 <= 1000:
+    skaitlis += 1
+print("Pirmāis skaitlis, kura kvadrāts ir lielāks par 1000, ir:", skaitlis)
